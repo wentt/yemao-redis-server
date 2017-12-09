3ccb4e1f4c2d9008b4662b3f086cde32741d5e27
wentt <769875543@qq.com>
2017-12-09 15:27:22 +0800

    first commit

---
+++ settings.js
@@ -0,0 +1,7 @@
+var settings={};
+settings = {
+    RDS_POST : '6379',
+    RDS_HOST : 'r-uf6d28c3db08f104.redis.rds.aliyuncs.com',
+    RDS_OPTS : {auth_pass:'Yx20171024'}
+}
+module.exports=settings;
